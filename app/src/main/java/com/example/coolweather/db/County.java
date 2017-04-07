8ea29e3a78fc789a825b47cd4543b22067478b1a
iknowi <471707554@qq.com>
2017-04-07 10:53:36 +0800

    First commit(modified)

--- app/src/main/java/com/example/coolweather/db/County.java
+++ app/src/main/java/com/example/coolweather/db/County.java
@@ -6,7 +6,7 @@ import org.litepal.crud.DataSupport;
  * Created by Administrator on 2017/4/6.
  */
 
-public class Country extends DataSupport {
+public class County extends DataSupport {
     private int id;
     private String countyName;
     private String weatherId;
